fff95b1318ca5957d0c2fd5431095e0768c8141c
andyred <andyred@umeyun.com>
2018-12-18 01:48:09 +0800

    测试branch Master 的代码事宜

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("hello world");
         System.out.println("测试GIT服务器更新");
+        System.out.println("测试MAST的内容");
+
     }
 }

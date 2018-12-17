fd3e6efe158af49f1bbba788bfee9fc5e11e64ba
andyred <andyred@umeyun.com>
2018-12-18 02:01:49 +0800

    test dev branch update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("hello world");
         System.out.println("测试GIT服务器更新");
         System.out.println("测试dev分支的的内容");
+        System.out.println("test merge");
     }
 }
